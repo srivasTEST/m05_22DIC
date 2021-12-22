1a948d3270de76370ba8e976e799be2432b14d98
Santi Rivas - Test <96136025+srivasTEST@users.noreply.github.com>
2021-12-22 09:17:52 +0100

    Update Project1.java

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -19,6 +19,8 @@ public class Project1 {
         System.out.println("Hello world!!!");
         
         System.out.println("Segona línia");
+        
+        //Aquest comentari l'he afegit desde GitHUB
     }
     
 }
